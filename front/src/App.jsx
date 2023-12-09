16b1bca8160ad94efc5a236fa0de4273edf076d5
joncoe <info@joncoe.com>
2023-12-09 13:11:26 +0900

    Aesthetic touch up

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -65,45 +65,47 @@ function App() {
 				<h1 className="mb-5 inline-block bg-white p-3">Space Vacation</h1>
 			</div>
 			<div className="flex justify-between max-w-5xl m-auto">
-				<div className="form  p-3 bg-white">
-					<form onSubmit={handleSubmit}>
-						<div className="field">
-							<div>
-								<label htmlFor="selectedPlanet">Choose a planet</label>
-							</div>
-
-							<label className="form-control w-full max-w-xs mb-5">
-								<div className="label">
-									<span className="label-text">
-										Where do you want to boldly go?
-									</span>
+				<div className="form">
+					<div className="  p-3 bg-white">
+						<form onSubmit={handleSubmit}>
+							<div className="field">
+								<div>
+									<label htmlFor="selectedPlanet">Choose a planet</label>
 								</div>
-								<select
-									className="select select-bordered"
-									name="selectedPlanet"
-									id="selectedPlanet"
-									onChange={handleChange}
-									defaultValue={'none'}
-								>
-									<option disabled value="none">
-										Choose a planet
-									</option>
-									{planetList.map((planet, i) => (
-										<option
-											key={planet.Planet + i}
-											name="planet"
-											value={planet.Planet}
-										>
-											{planet.Planet}
+
+								<label className="form-control w-full max-w-xs mb-5">
+									<div className="label">
+										<span className="label-text">
+											Where do you want to boldly go?
+										</span>
+									</div>
+									<select
+										className="select select-bordered"
+										name="selectedPlanet"
+										id="selectedPlanet"
+										onChange={handleChange}
+										defaultValue={'none'}
+									>
+										<option disabled value="none">
+											Choose a planet
 										</option>
-									))}
-								</select>
-							</label>
-						</div>
-						<button type="submit" className="btn btn-primary">
-							Let's go
-						</button>
-					</form>
+										{planetList.map((planet, i) => (
+											<option
+												key={planet.Planet + i}
+												name="planet"
+												value={planet.Planet}
+											>
+												{planet.Planet}
+											</option>
+										))}
+									</select>
+								</label>
+							</div>
+							<button type="submit" className="btn btn-primary">
+								Let's go
+							</button>
+						</form>
+					</div>
 				</div>
 				<div className=" p-3 w-1/4 font-mono text-sm">
 					{selectedPlanet && (
