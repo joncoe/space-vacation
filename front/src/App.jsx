e8a97023794a9db7ec2b93d44ba2007008d0370a
joncoe <info@joncoe.com>
2023-12-10 12:47:10 +0900

    Add Privacy link

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import PageSelectPlanet from './pages/PageSelectPlanet';
 import { Routes, Route } from 'react-router-dom';
 import './App.scss';
@@ -5,6 +6,7 @@ import AboutUs from './pages/About';
 import Booking from './pages/Booking';
 import Login from './pages/Login';
 import Admin from './pages/Admin';
+import Privacy from './pages/Privacy';
 import Navigation from './components/Navigation';
 import LostInSpace from './pages/LostInSpace';
 
@@ -18,23 +20,29 @@ function App() {
 				<Route path="/booking" element={<Booking />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/admin" element={<Admin />} />
+				<Route path="/privacy" element={<Privacy />} />
 				<Route path="*" element={<LostInSpace />} />
 			</Routes>
-			<p className="copyright">
-				Photos from{' '}
-				<a href="http://nasa.gov" target="_blank" rel="noreferrer">
-					NASA
-				</a>
-				,{' '}
-				<a
-					href="https://asdc.larc.nasa.gov/copyright-information#:~:text=Unless%20otherwise%20noted%2C%20all%20images,exhibits%2C%20and%20Internet%20web%20pages."
-					target="_blank"
-					rel="noreferrer"
-				>
-					available for general use
-				</a>
-				.
-			</p>
+			<footer>
+				<p className="copyright">
+					Photos from{' '}
+					<a href="http://nasa.gov" target="_blank" rel="noreferrer">
+						NASA
+					</a>
+					,{' '}
+					<a
+						href="https://asdc.larc.nasa.gov/copyright-information#:~:text=Unless%20otherwise%20noted%2C%20all%20images,exhibits%2C%20and%20Internet%20web%20pages."
+						target="_blank"
+						rel="noreferrer"
+					>
+						available for general use
+					</a>
+					.
+				</p>
+				<p className="className copyright privacy">
+					<Link to="/privacy">Privacy Policy</Link>
+				</p>
+			</footer>
 		</>
 	);
 }
--- front/src/pages/Privacy.jsx
+++ front/src/pages/Privacy.jsx
@@ -1,13 +1,20 @@
-import './About.scss';
-function AboutUs() {
+import './Privacy.scss';
+function Privacy() {
 	return (
 		<>
 			<div className="max-w-5xl m-auto mt-10">
 				<h1 className="mb-5 inline-block bg-white p-3">Privacy Policy</h1>
 			</div>
-			<div>We won't tell anyone.</div>
+			<div>
+				<p>
+					We won't tell anyone what you are up to. But you know the day will
+					come when we get an offer too good to refuse to sell your data. And we
+					will. Because that is what startups do. By the way, do you want to
+					work here? We will pay you in sweet delicious equity!
+				</p>
+			</div>
 		</>
 	);
 }
 
-export default AboutUs;
+export default Privacy;
