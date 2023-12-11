99eb538dd5be0505a55e8445911fc8788ef318cc
joncoe <info@joncoe.com>
2023-12-11 21:28:58 +0900

    fix header text style

--- front/src/pages/LostInSpace.jsx
+++ front/src/pages/LostInSpace.jsx
@@ -3,7 +3,7 @@ function LostInSpace() {
 	return (
 		<>
 			<div className="max-w-lg m-auto mt-20">
-				<h1 className=".four-oh-four">404</h1>
+				<h1 className="four-oh-four">404</h1>
 				<p className="blackhole">You have fallen past the event horizon.</p>
 			</div>
 			<div className="background-image bg-image"></div>
