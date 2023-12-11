55d5824ef566f12c82a50b214ce6dc8f0b79a864
joncoe <info@joncoe.com>
2023-12-11 19:45:30 +0900

    Add privacy

--- front/src/pages/Privacy.jsx
+++ front/src/pages/Privacy.jsx
@@ -2,16 +2,118 @@ import './Privacy.scss';
 function Privacy() {
 	return (
 		<>
-			<div className="max-w-5xl m-auto mt-10">
+			<div className="max-w-lg m-auto mt-10">
 				<h1 className="mb-5 inline-block bg-white p-3">Privacy Policy</h1>
 			</div>
-			<div>
+			<div className="max-w-lg m-auto mt-10">
 				<p>
 					We won't tell anyone what you are up to. But you know the day will
 					come when we get an offer too good to refuse to sell your data. And we
-					will. Because that is what startups do. By the way, do you want to
-					work here? We will pay you in sweet delicious equity!
+					will. In the meantime,
 				</p>
+
+				<p>
+					We are committed to ensuring the privacy and security of your personal
+					information. This Privacy Policy outlines how we collect, use,
+					disclose, and protect the information we gather when you interact with
+					our website, services, or purchase our interplanetary travel packages.
+					By using our services, you agree to the practices described in this
+					policy.
+				</p>
+
+				<h2>Information We Collect</h2>
+
+				<p>
+					We may collect various types of information from users to provide and
+					improve our services:
+				</p>
+
+				<ol>
+					<li>
+						<strong>Personal Information:</strong> When you make a purchase or
+						create an account, we may collect your name, contact details,
+						billing information, and any other information necessary to process
+						your orders or bookings.
+					</li>
+					<li>
+						<strong>Device and Usage Information:</strong> We automatically
+						collect information about your device, browsing actions, and
+						patterns when you visit our website or use our services. This may
+						include your IP address, browser type, pages visited, and other
+						usage data.
+					</li>
+					<li>
+						<strong>Communication Data:</strong> We may retain records of your
+						correspondence with us, including emails or messages sent through
+						our platform, to help address your inquiries or improve our
+						services.
+					</li>
+				</ol>
+
+				<h2>How We Use Your Information</h2>
+
+				<p>We use the information collected for various purposes, including:</p>
+
+				<ul>
+					<li>Service Delivery</li>
+					<li>Personalization</li>
+					<li>Communication</li>
+					<li>Analytics and Improvement</li>
+				</ul>
+
+				<h2>Information Sharing and Disclosure</h2>
+
+				<p>
+					We do not sell, trade, or rent your personal information to third
+					parties for their marketing purposes. However, we may share your
+					information in certain circumstances, such as:
+				</p>
+
+				<ul>
+					<li>Service Providers</li>
+					<li>Legal Compliance and Protection</li>
+				</ul>
+
+				<h2>Data Security</h2>
+
+				<p>
+					We implement appropriate technical and organizational measures to
+					safeguard your personal information against unauthorized access,
+					disclosure, alteration, or destruction.
+				</p>
+
+				<h2>Your Rights</h2>
+
+				<p>
+					You have the right to access, correct, update, or delete your personal
+					information. You can also opt-out of receiving marketing
+					communications from us at any time.
+				</p>
+
+				<h2>Children’s Privacy</h2>
+
+				<p>
+					Our services are not intended for individuals under the age of 18. We
+					do not knowingly collect personal information from children.
+				</p>
+
+				<h2>Changes to this Privacy Policy</h2>
+
+				<p>
+					We may update this Privacy Policy periodically to reflect changes in
+					our practices or legal requirements. We encourage you to review this
+					policy periodically for any updates.
+				</p>
+
+				<h2>Contact Us</h2>
+
+				<p>
+					If you have any questions, concerns, or requests regarding this
+					Privacy Policy or our privacy practices, please contact us at [Contact
+					Information].
+				</p>
+
+				<p>This Privacy Policy was last updated on [Date].</p>
 			</div>
 		</>
 	);
