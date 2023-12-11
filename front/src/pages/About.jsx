e78d4fe70a9e9c073e00e8fa93c2e3972f290e14
joncoe <info@joncoe.com>
2023-12-11 19:46:09 +0900

    Fix up About

--- front/src/pages/About.jsx
+++ front/src/pages/About.jsx
@@ -2,14 +2,17 @@ import './About.scss';
 function AboutUs() {
 	return (
 		<>
-			<div className="max-w-5xl m-auto mt-10">
-				<h1 className="mb-5 inline-block bg-white p-3">About Us</h1>
+			<div>
+				<div className="max-w-xl m-auto mt-10">
+					<h1 className="mb-5 inline-block bg-white p-3">About Us</h1>
+				</div>
+				<div className="max-w-xl m-auto mt-10 bg-white p-3">
+					We are a tiny travel agency with a big vision. We will take you to the
+					stars! Actually, not that far. We will take you to any planet in our
+					Solar System that we currently service.
+				</div>
 			</div>
-			<div className="max-w-5xl m-auto mt-10">
-				We are a tiny travel agency with a big vision. We will take you to the
-				stars! Actually, not that far. We will take you to any planet in our
-				Solar System that we currently service.
-			</div>
+			<div className="background-image bg-image-about"></div>
 		</>
 	);
 }
